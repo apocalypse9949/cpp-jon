0e8fe14d8dd67d85cb77962d9502e0b4215c9c38
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-08-25 17:21:43 +0530

    Create pointer2.cpp

---
+++ pointer2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+int m =10,n,o; 
+int *z = &m;
+cout<<z<<endl;
+cout<<*z<<endl;
+cout<<&m<<endl;
+cout<<&n<<endl;
+cout<<&o<<endl;
+cout<<&z<<endl;
+return 0;
+}
