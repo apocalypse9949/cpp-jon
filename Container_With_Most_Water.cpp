990ad1f7b92dee14fa2c0f1f06a5ddada7483c01
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-10-14 11:53:43 +0530

    Update Container_With_Most_Water.cpp
    two-pointer approach

--- Container_With_Most_Water.cpp
+++ Container_With_Most_Water.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0; int right = height.size() - 1 ;
-        int maxi =0;
+        int maxi =0; 
         while(left<right){
             int h = min(height[left], height[right]);
             int width = right - left;
