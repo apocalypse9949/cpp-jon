23559cfa94cbac6d211f0482cc1a28cdaeca9897
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-11-01 10:30:53 +0530

    Refactor node removal logic in linked list with hashset

--- Delete_Nodes_From_Linked_List_Present_in_Array.cpp
+++ Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -1,37 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        // Create an unordered_set for efficient lookup of values in nums
-        unordered_set<int> valuesToRemove(nums.begin(), nums.end());
+        unordered_set<int> valuestoremove(nums.begin(),nums.end());
+        
+        while(head != nullptr && valuestoremove.count(
+            head->val) > 0)
+{
+    ListNode* temp = head;
+    head = head->next;
 
-        // Handle the case where the head node needs to be removed
-        while (head != nullptr && valuesToRemove.count(head->val) > 0) {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-        }
-
-        // If the list is empty after removing head nodes, return nullptr 
-        if (head == nullptr) {
-            return nullptr;
-        }
-
-        // Iterate through the list, removing nodes with values in the set
-        ListNode* current = head;
-        while (current->next != nullptr) {
-            if (valuesToRemove.contains(current->next->val)) {
-                // Store the node to be deleted
-                ListNode* temp = current->next;
-                // Skip the next node by updating the pointer
-                current->next = current->next->next;
-                // Delete the removed node
-                delete temp;
-            } else {
-                // Move to the next node
-                current = current->next;
-            }
-        }
-
-        return head;
+}
+if(head == nullptr){
+    return nullptr;
+}
+ListNode* current = head;
+while(current->next != nullptr){
+    if(valuestoremove.count(current->next->val)){
+    ListNode* temp = current->next;
+    current->next = current->next->next;
+    
+}else{
+    current = current->next;
+}
+}        
+return head;
     }
 };
