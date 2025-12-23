4e321d774a29ec58666b9d809ebdd040d269ff59
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-12-23 12:13:03 +0530

    Implement min_coins.cpp to calculate minimum coins

---
+++ min_coins.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int findMin(int n)
+{
+    int count=0; 
+    vector<int> denomination = { 1, 2, 5, 10 };
+    for (int i = denomination.size() - 1; i >= 0; i--) {
+      count+=n/denomination[i];
+      n=n%denomination[i];
+   
+    }
+    return count; 
+}
+
+int main()
+{
+   
+    int n ;
+  cin >> n;
+    cout<<findMin(n);
+    return 0;
+}
